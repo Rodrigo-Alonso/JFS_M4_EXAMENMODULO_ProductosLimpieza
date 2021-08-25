8f65a504613dbadd2d5c2bca3b1d733e3115adfd
rodrigo_alonso <rodrigo.alonso.pereira@gmail.com>
2021-08-25 14:33:16 -0400

    implementacion ProductoDAO.save

--- src/main/java/cl/edutecno/dto/UsuarioDTO.java
+++ src/main/java/cl/edutecno/dto/UsuarioDTO.java
@@ -1,5 +1,50 @@
 package cl.edutecno.dto;
 
 public class UsuarioDTO {
-
+	
+	private int idUsuario;
+	private String nombreUsuario;
+	private String correoUsuario;
+	private String telefonoUsuario;
+	private String PassUsuario;
+	
+	//G&S
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getCorreoUsuario() {
+		return correoUsuario;
+	}
+	public void setCorreoUsuario(String correoUsuario) {
+		this.correoUsuario = correoUsuario;
+	}
+	public String getTelefonoUsuario() {
+		return telefonoUsuario;
+	}
+	public void setTelefonoUsuario(String telefonoUsuario) {
+		this.telefonoUsuario = telefonoUsuario;
+	}
+	public String getPassUsuario() {
+		return PassUsuario;
+	}
+	public void setPassUsuario(String passUsuario) {
+		PassUsuario = passUsuario;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "UsuarioDTO [idUsuario=" + idUsuario + ", nombreUsuario=" + nombreUsuario + ", correoUsuario="
+				+ correoUsuario + ", telefonoUsuario=" + telefonoUsuario + ", PassUsuario=" + PassUsuario + "]";
+	}
+	
 }
--- src/main/java/cl/edutecno/facade/Facade.java
+++ src/main/java/cl/edutecno/facade/Facade.java
@@ -1,5 +1,17 @@
 package cl.edutecno.facade;
 
-public class Facade {
+import cl.edutecno.dao.ProductoDAO;
+import cl.edutecno.dto.ProductoDTO;
 
+public class Facade {
+	
+	public int registrarProducto(ProductoDTO productoDTO) {
+		ProductoDAO dao = new ProductoDAO();
+		return dao.save(productoDTO);
+	}
+
+	public int actualizarProducto(ProductoDTO productoDTO) {
+		
+		return 0;
+	}
 }
